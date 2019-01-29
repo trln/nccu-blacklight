c16b4212f434ea438408079fe8f55ea116b68cba
Genia Kazymova <genia@trln.org>
2019-01-29 18:14:42 +0000

    Set ActionMailer up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "nccu-blacklight_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => "email-smtp.us-east-1.amazonaws.com",
+    :port => 587,
+    :user_name => ENV["SES_SMTP_USERNAME"],
+    :password => ENV["SES_SMTP_PASSWORD"],
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_options = {
+    from: 'admin@trln.org'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
